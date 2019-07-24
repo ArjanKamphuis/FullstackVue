c72a4f059490c1816e82dd55bdc33eba8530d644
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2019-07-24 19:27:30 +0200

    calendar_app

---
+++ calendar_app/src/app/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <CalendarWeek />
+        <CalendarEntry />
+    </div>
+</template>
+
+<script>
+    import CalendarWeek from './components/CalendarWeek.vue';
+    import CalendarEntry from './components/CalendarEntry.vue';
+    export default {
+        name: 'App',
+        components: {
+            CalendarWeek,
+            CalendarEntry
+        }
+    }
+</script>
+
+<style lang="scss">
+    html, body {
+        height: 100%;
+    }
+</style>
+
+<style lang="scss" scoped>
+    #app {
+        height: inherit;
+        background: #6E6E6E;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
