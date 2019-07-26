f2e451cc5be8e0647b6e56f68caf276a6b4c431e
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2019-07-26 14:27:26 +0200

    Form Handling

---
+++ form_handling/app/01-basic-button/main.js
@@ -0,0 +1,25 @@
+const buttonRow = {
+    template: `
+        <div>
+            <button @click="onButtonClick" name="button-hoodie" value="fullstack-hoodie" class="ui button">Hoodie</button>
+            <button @click="onButtonClick" name="button-tee" value="fullstack-tee" class="ui button">Tee</button>
+            <button @click="onButtonClick" name="button-fitted-cap" value="fullstack-fitted-cap" class="ui button">Fitted Cap</button>
+            <button @click="onButtonClick" name="button-jacket" value="fullstack-jacket" class="ui button">Jacket</button>
+        </div>
+    `,
+    methods: {
+        onButtonClick(e) {
+            const button = e.target;
+            console.log(`The user clicked ${button.name}: ${button.value}`);
+        }
+    }
+}
+
+window.addEventListener('load', () => {
+    new Vue({
+        el: '#app',
+        components: {
+            'button-row': buttonRow
+        }
+    });
+});
